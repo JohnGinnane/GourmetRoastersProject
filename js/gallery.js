2be1f82816b261acefbefe00d4679c9990899ef1
John Ginnane <John.Ginnane1@Gmail.com>
2024-04-17 22:30:33 +0100

    Optimised beans, i'll try spinning thats a good trick

--- js/gallery.js
+++ js/gallery.js
@@ -10,38 +10,39 @@ document.addEventListener("DOMContentLoaded", function() {
         x++;
             
         if (x % rate == 0 && x != 0){
-            var letter = {};
+            var bean = {};
             //letter.char = String.fromCharCode(65 + Math.round(Math.random() * 25));
-            letter.life = maxlife;
-            //letter.colour = tinycolor("hsl(27, 100%, " + Math.round(20 + Math.random() * 15) + "%)").toHexString();
-            letter.colour = Math.round(20 + Math.random() * 15);
-            letter.x = e.pageX - 32;
-            letter.y = e.pageY - 100;
-            letter.velY = Math.random() * -1;
-            letter.velX = (((Math.random() * 2) - 1) * 0.5);
-            letter.angle = Math.random() * 360;
+            bean.life = maxlife;
+            //bean.colour = tinycolor("hsl(27, 100%, " + Math.round(20 + Math.random() * 15) + "%)").toHexString();
+            bean.colour = Math.round(20 + Math.random() * 15);
+            bean.x = e.pageX - 32;
+            bean.y = e.pageY - 100;
+            bean.velY = randomNumber(0, -0.5);
+            bean.velX = randomNumber(-0.2, 0.2)
+            bean.angle = randomNumber(0, 360);
+            bean.angularVelocity = randomNumber(-0.5, 0.5);
             let scale = Math.random() * 8;
-            letter.width = 24 + scale;
-            letter.height = 24 + scale;
-            
-            letter.html = function() {
-                //return "<span style='overflow: hidden; position:absolute; font-size: " + (letter.life / 10) + "px; color:" + letter.colour + "; top:" + letter.y + "px; left:" + letter.x + "px;'>" + letter.char + "</span>";
-                let clonedBean = document.querySelector("svg.svg-bean").cloneNode(true);
-                clonedBean.style.position = "absolute"
-                clonedBean.style.top = letter.y + "px";
-                clonedBean.style.left = letter.x + "px";
-                clonedBean.style.display = "block";
-                clonedBean.setAttribute("width", letter.width);
-                clonedBean.setAttribute("height", letter.height);
-                clonedBean.setAttribute("transform", "rotate(" + letter.angle + ")");
-                //clonedBean.querySelector("g>path").style.fill = "rgba(255, 0, 0, " + (letter.life / maxlife) + ")"
-                //clonedBean.querySelector("g>path").style.fill = letter.colour;
-                clonedBean.querySelector("g>path").style.fill = tinycolor("hsla(27, 100%, " + letter.colour + "%, " + (letter.life / maxlife) + ")").toHexString();
+            bean.width = 24 + scale;
+            bean.height = 24 + scale;
 
-                return clonedBean.outerHTML;
+            // Clone node and set aesthetics ONCE
+            bean.node = document.querySelector("svg.svg-bean").cloneNode(true);
+            bean.node.style.position = "absolute";
+            bean.node.querySelector("g>path").style.fill = tinycolor("hsl(27, 100%, " + bean.colour + "%)");
+            bean.node.setAttribute("width", bean.width);
+            bean.node.setAttribute("height", bean.height);
+            bean.node.style.display = "block";
+            bean.node.setAttribute("transform", "rotate(" + bean.angle + ")");
+
+            bean.html = function() {
+                // Each time we call the code we need to update position
+                bean.node.style.top = bean.y + "px";
+                bean.node.style.left = bean.x + "px";
+                bean.node.setAttribute("transform", "rotate(" + bean.angle + ")");
+                return bean.node.outerHTML;
             }
             
-            beans.push(letter);
+            beans.push(bean);
         }
 
         if (x >= 255) { x = 0; }
@@ -53,28 +54,34 @@ document.addEventListener("DOMContentLoaded", function() {
     setInterval(update);
 });
 
+function randomNumber(min, max) {
+    return Math.random() * (max - min) + min;
+}
+
 function update(){
     let delta = new Date() - lastTime;
     lastTime = new Date();
 
     let out = '';
     
-    for(i = 0; i < beans.length; i++)
-    {               
-        beans[i].life = beans[i].life - (0.2 * delta);
+    for(i = 0; i < beans.length; i++) {               
+        let bean = beans[i];
+
+        bean.life = bean.life - (0.2 * delta);
         
-        if (beans[i].life <= 0){
+        if (bean.life <= 0){
             beans.splice(i, 1);            
             out = '';            
             continue;
         }
         
-        //beans[i].y = beans[i].y + (delta * 0.1 + (0.01 * (beans[i].life * 0.5)));
-        beans[i].velY += delta * 0.004;
-        beans[i].y += beans[i].velY;
-        beans[i].x += beans[i].velX;
-        
-        out += beans[i].html();
+        //bean.y = bean.y + (delta * 0.1 + (0.01 * (bean.life * 0.5)));
+        bean.velY += delta * 0.002;
+        bean.y += bean.velY * delta;
+        bean.x += bean.velX * delta;
+        bean.angle += bean.angularVelocity * delta;
+    
+        out += bean.html();
     }
     
     $("#background").html(out);
