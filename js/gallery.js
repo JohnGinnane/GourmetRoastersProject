2ca478c3f0a714b58895bf5f0745e14152980fe8
John Ginnane <John.Ginnane1@Gmail.com>
2024-04-17 22:52:36 +0100

    Refactored beans, improve performance, made pool

--- js/gallery.js
+++ js/gallery.js
@@ -1,6 +1,8 @@
 let beans = [];
 const rate = 50;
 const maxlife = 500;
+// Use pooling to improve performance
+const maxBeans = 50;
 let x = 0;
 let lastTime = new Date();
 
@@ -10,39 +12,7 @@ document.addEventListener("DOMContentLoaded", function() {
         x++;
             
         if (x % rate == 0 && x != 0){
-            var bean = {};
-            //letter.char = String.fromCharCode(65 + Math.round(Math.random() * 25));
-            bean.life = maxlife;
-            //bean.colour = tinycolor("hsl(27, 100%, " + Math.round(20 + Math.random() * 15) + "%)").toHexString();
-            bean.colour = Math.round(20 + Math.random() * 15);
-            bean.x = e.pageX - 32;
-            bean.y = e.pageY - 100;
-            bean.velY = randomNumber(0, -0.5);
-            bean.velX = randomNumber(-0.2, 0.2)
-            bean.angle = randomNumber(0, 360);
-            bean.angularVelocity = randomNumber(-0.5, 0.5);
-            let scale = Math.random() * 8;
-            bean.width = 24 + scale;
-            bean.height = 24 + scale;
-
-            // Clone node and set aesthetics ONCE
-            bean.node = document.querySelector("svg.svg-bean").cloneNode(true);
-            bean.node.style.position = "absolute";
-            bean.node.querySelector("g>path").style.fill = tinycolor("hsl(27, 100%, " + bean.colour + "%)");
-            bean.node.setAttribute("width", bean.width);
-            bean.node.setAttribute("height", bean.height);
-            bean.node.style.display = "block";
-            bean.node.setAttribute("transform", "rotate(" + bean.angle + ")");
-
-            bean.html = function() {
-                // Each time we call the code we need to update position
-                bean.node.style.top = bean.y + "px";
-                bean.node.style.left = bean.x + "px";
-                bean.node.setAttribute("transform", "rotate(" + bean.angle + ")");
-                return bean.node.outerHTML;
-            }
-            
-            beans.push(bean);
+            createBean(e.pageX - 32, e.pageY - 100);
         }
 
         if (x >= 255) { x = 0; }
@@ -54,34 +24,95 @@ document.addEventListener("DOMContentLoaded", function() {
     setInterval(update);
 });
 
+function createBean(x, y) {
+    let foundBean = false;
+
+    // Try to find an inactive bean
+    for (let i = 0; i < beans.length; i++) {
+        if (!beans[i]) { continue; }
+
+        if (!beans[i].active) {
+            foundBean = beans[i];
+            break;
+        }
+    }
+
+    // If we couldn't find an inactive bean then we need to create a new one
+    if (!foundBean && beans.length < maxBeans) {
+        // Create a new bean
+        foundBean = {};
+        // Clone node and set aesthetics ONCE
+        foundBean.html = function() {
+            // Each time we call the code we need to update position
+            foundBean.node.style.top = foundBean.y + "px";
+            foundBean.node.style.left = foundBean.x + "px";
+            foundBean.node.setAttribute("transform", "rotate(" + foundBean.angle + ")");
+            return foundBean.node.outerHTML;
+        }
+
+        foundBean.node = document.querySelector("svg.svg-bean").cloneNode(true);
+        beans.push(foundBean);
+    }
+    
+    // With our bean lets reset position, colour, etc.
+    if (foundBean) {
+        foundBean.active = true;
+        foundBean.life = maxlife;
+        foundBean.colour = Math.round(20 + Math.random() * 15);
+        foundBean.x = x;
+        foundBean.y = y;
+        foundBean.velY = randomNumber(0, -0.5);
+        foundBean.velX = randomNumber(-0.2, 0.2)
+        foundBean.angle = randomNumber(0, 360);
+        foundBean.angularVelocity = randomNumber(-0.5, 0.5);
+        let scale = Math.random() * 8;
+        foundBean.width = 24 + scale;
+        foundBean.height = 24 + scale;
+
+        foundBean.node.style.position = "absolute";
+        foundBean.node.querySelector("g>path").style.fill = tinycolor("hsl(27, 100%, " + foundBean.colour + "%)");
+        foundBean.node.setAttribute("width", foundBean.width);
+        foundBean.node.setAttribute("height", foundBean.height);
+        foundBean.node.style.display = "block";
+        foundBean.node.setAttribute("transform", "rotate(" + foundBean.angle + ")");
+    }
+}
+
 function randomNumber(min, max) {
     return Math.random() * (max - min) + min;
 }
 
 function update(){
+    // Need to get the time since the last update
+    // to make physics consistent across machines
     let delta = new Date() - lastTime;
     lastTime = new Date();
 
     let out = '';
     
-    for(i = 0; i < beans.length; i++) {               
+    // Iterate over all beans, and:
+    // 1. Reduce life
+    // 2. Make inactive if necessary
+    // 3. Change position based on velocity
+    // 4. Change velocity based on gravity
+    for(i = 0; i < beans.length; i++) {
         let bean = beans[i];
 
-        bean.life = bean.life - (0.2 * delta);
-        
-        if (bean.life <= 0){
-            beans.splice(i, 1);            
-            out = '';            
+        if (bean.life <= 0 && bean.active) {
+            bean.active = false;
             continue;
         }
         
-        //bean.y = bean.y + (delta * 0.1 + (0.01 * (bean.life * 0.5)));
-        bean.velY += delta * 0.002;
-        bean.y += bean.velY * delta;
-        bean.x += bean.velX * delta;
-        bean.angle += bean.angularVelocity * delta;
-    
-        out += bean.html();
+        if (bean.active) {
+            bean.life = bean.life - (0.2 * delta);
+            
+            bean.velY += delta * 0.002;
+            bean.y += bean.velY * delta;
+            bean.x += bean.velX * delta;
+            bean.angle += bean.angularVelocity * delta;
+        
+            out += bean.html();
+        }
     }
     
     $("#background").html(out);
