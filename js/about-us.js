9b1393f827b59cbbe7d6a6822c0f7bef1ecf10ea
John Ginnane <John.Ginnane1@Gmail.com>
2024-04-18 19:13:02 +0100

    Added validation to pop quiz submit

--- js/about-us.js
+++ js/about-us.js
@@ -2,22 +2,61 @@ let modalQuizResult;
 
 document.addEventListener("DOMContentLoaded", function() {
     modalQuizResult = document.getElementById('modal-about-us-quiz-result');
-    //openQuizResults();
+    
+    // Added event to the pop quiz radio buttons
+    // Only if all four are filled in will we enable submit button
+    let radioButtons = this.getElementsByClassName("pop-quiz-radio");
+    
+    for (let button of radioButtons) {
+        button.addEventListener("click", function() {
+            radioButton_OnClick(button);
+        });
+    }
+
+    radioButton_OnClick(null);
 });
 
-function popQuizSubmit() {
-    // Retrieve the answers from all radio buttons
-    let q1answer = getRadioGroupSelection("pop-quiz-question-1").toLowerCase();
-    let q2answer = getRadioGroupSelection("pop-quiz-question-2").toLowerCase();
-    let q3answer = getRadioGroupSelection("pop-quiz-question-3").toLowerCase();
-    let q4answer = getRadioGroupSelection("pop-quiz-question-4").toLowerCase();
-    
-    let score = 0;
+function getAnswers() {
+    let answers = [];
 
-    if (q1answer == "morocco")   { score++; }
-    if (q2answer == "1991")      { score++; }
-    if (q3answer == "classes")   { score++; }
-    if (q4answer == "spearmint") { score++; }
+    answers[0] = getRadioGroupSelection("pop-quiz-question-1").toLowerCase();
+    answers[1] = getRadioGroupSelection("pop-quiz-question-2").toLowerCase();
+    answers[2] = getRadioGroupSelection("pop-quiz-question-3").toLowerCase();
+    answers[3] = getRadioGroupSelection("pop-quiz-question-4").toLowerCase();
+
+    return answers;
+}
+
+function radioButton_OnClick(sender) {
+    // Check if all answers are filled in
+    let answers = getAnswers();
+    let submitEnabled = true;
+
+    // If any answer is left blank then do nothing
+    for (let answer of answers) {
+        if (answer == "") {
+            submitEnabled = false;
+            break;
+        }
+    }
+   
+    // Else make the submit button visible
+    document.getElementById("pop-quiz-submit").disabled = !submitEnabled;
+}
+
+function popQuizSubmit() {
+    let score = 0;
+    let answers = getAnswers();
+
+    // If any answer is left blank then do nothing
+    for (let answer of answers) {
+        if (answer == "") { return ; }
+    }
+    
+    if (answers[0] == "morocco")   { score++; }
+    if (answers[1] == "1991")      { score++; }
+    if (answers[2] == "classes")   { score++; }
+    if (answers[3] == "spearmint") { score++; }
     
     openQuizResults(score);
 }
@@ -44,6 +83,7 @@ function openQuizResults(score) {
             break;
     }
 
+    // Set the right message on the coffee cup label
     let spanCoffeeCupLabel = document.getElementById("span-coffee-cup-label");
     spanCoffeeCupLabel.innerHTML = strResult;
 
